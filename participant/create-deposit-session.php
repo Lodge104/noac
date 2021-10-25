1ca8913ac0121753a9b5defa3a019b84a30a617b
Nicholas Anderson <nickanderson1998@gmail.com>
2021-10-25 13:52:57 -0400

    Update create-deposit-session.php

--- participant/create-deposit-session.php
+++ participant/create-deposit-session.php
@@ -1,37 +1,17 @@
 <?php
-use Slim\Http\Request;
-use Slim\Http\Response;
-use Stripe\Stripe;
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App;
-
-$app->add(function ($request, $response, $next) {
-  \Stripe\Stripe::setApiKey(getenv('STRIPEKEY'));
-  return $next($request, $response);
-});
-
-$app->post('/create-deposit-session', function (Request $request, Response $response) {
-  $session = \Stripe\Checkout\Session::create([
-    'payment_method_types' => ['card'],
-    'line_items' => [[
-      'price_data' => [
-        'currency' => 'usd',
-        'product_data' => [
-          'name' => 'T-shirt',
-        ],
-        'unit_amount' => 2000,
-      ],
-      'quantity' => 1,
-    ]],
-    'mode' => 'payment',
-    'success_url' => 'https://example.com/success',
-    'cancel_url' => 'https://example.com/cancel',
-  ]);
-
-  return $response->withHeader('Location', $session->url)->withStatus(303);
-});
-
-$app->run();
-?>
+$stripe = new \Stripe\StripeClient(getenv('STRIPEKEY'));
+$stripe->checkout->sessions->create([
+  'success_url' => 'https://example.com/success',
+  'cancel_url' => 'https://example.com/cancel',
+  'payment_method_types' => ['card'],
+  'line_items' => [
+    [
+      'price' => 'price_H5ggYwtDq4fbrJ',
+      'quantity' => 2,
+    ],
+  ],
+  'mode' => 'payment',
+]);
