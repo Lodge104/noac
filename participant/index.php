ce793f146b1871e3b51b4315f210a6e5c111d25f
Nicholas Anderson <nickanderson1998@gmail.com>
2021-10-25 20:47:30 -0400

    Style

--- participant/index.php
+++ participant/index.php
@@ -22,6 +22,7 @@ $host = $_SERVER['SERVER_NAME'];
   <link rel="stylesheet" href="../libraries/fontawesome-free-5.12.0/css/all.min.css">
   <link rel="stylesheet" href="https://use.typekit.net/awb5aoh.css" media="all">
   <link rel="stylesheet" href="../style.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
 
 </head>
 
