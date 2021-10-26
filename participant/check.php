444a217af83aec86e1ba6628e72f5918bbf5c647
Nicholas Anderson <nickanderson1998@gmail.com>
2021-10-25 21:19:02 -0400

    Fix

--- participant/check.php
+++ participant/check.php
@@ -25,9 +25,38 @@ if (isset($_GET['bsaID'])) {
     $getbsaIDQuery->execute();
     $getbsaIDQ = $getbsaIDQuery->get_result();
     if ($getbsaIDQ->num_rows > 0) {
+
+        $url = ($transactionURL . $bsaID);
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $headers = array(
+          "Accept: application/json",
+          ("Authorization: Bearer " . $bearer),
+        );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        //for debug only!
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $resp = curl_exec($curl);
+        if (!curl_errno($curl)) {
+          switch ($http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE)) {
+            case 200:  # OK
+              break;
+            default:
+          }
+        }
+        curl_close($curl);
+
+        $json = json_decode($resp, true);
+        $_SESSION['transactions'] = $json;
+
     header("Location: index.php?bsaID=" . $bsaID);
     } else {
-        $url = ("https://registration-test.lodge104.net/api/members/" . $bsaID);
+        $url = ($membersURL . $bsaID);
 
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
--- participant/index.php
+++ participant/index.php
@@ -81,32 +81,7 @@ $host = $_SERVER['SERVER_NAME'];
 
       if (isset($_GET['bsaID'])) {
         $bsaID = $_POST['bsaID'] = $_GET['bsaID'];
-
-        $url = ("https://registration-test.lodge104.net/api/transactions/" . $bsaID);
-
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-        $headers = array(
-          "Accept: application/json",
-          ("Authorization: Bearer " . $bearer),
-        );
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        //for debug only!
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-        $resp = curl_exec($curl);
-        if (!curl_errno($curl)) {
-          switch ($http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE)) {
-            case 200:  # OK
-              break;
-            default:
-          }
-        }
-        curl_close($curl);
-        $json = json_decode($resp, true);
+        $json = $_SESSION['transactions'];
         $transactions = $json['transactions'];
         $sku = array_column($transactions, 'sku');
 
