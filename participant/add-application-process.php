ce1892999347304eca0919090d2f35a354c08172
Nicholas Anderson <nickanderson1998@gmail.com>
2021-10-27 22:01:09 -0400

    Update add-application-process.php

--- participant/add-application-process.php
+++ participant/add-application-process.php
@@ -53,7 +53,7 @@ $createAdult->close();
 if ($payment == '1') { $poption = 'Option 1';} else { $poption = 'Option 2';}
 
 //explode the date to get month, day and year
-$birthDate = explode("/", $dob);
+$birthDate = explode("-", $dob);
 //get age from date or birthdate
 $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
   ? ((date("Y") - $birthDate[2]) - 1)
