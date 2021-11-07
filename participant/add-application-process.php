9f1f1b2ae35eedfce6657e9af3ba4b93ca819aac
Nicholas Anderson <nickanderson1998@gmail.com>
2021-11-07 16:33:25 -0500

    Update add-application-process.php

--- participant/add-application-process.php
+++ participant/add-application-process.php
@@ -187,24 +187,24 @@ $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birt
 //   }
 
 
-  $ch = curl_init();
+$ch = curl_init();
 
-  curl_setopt($ch, CURLOPT_URL, 'https://api.mailersend.com/v1/email');
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, "{\n    \"from\": {\n        \"email\": \"noac@lodge104.com\"\n    },\n    \"to\": [\n        {\n            \"email\": \"nickanderson1998@gmail.com\"\n        }\n    ],\n    \"variables\": [{\n        \"email\": \"nickanderson1998@gmail.com\",\n        \"substitutions\": [\n            {\n                \"var\": \"age\",\n                \"value\": \"\"\n            },\n            {\n                \"var\": \"dob\",\n                \"value\": \"\"\n            },\n            {\n                \"var\": \"zip\",\n                \"value\": \"\"\n            },\n            {\n                \"var\": \"city\",\n                \"value\": \"\"\n            },\n            {\n                \"var\": \"bsaid\",\n                \"value\": \"\"\n            },\n            {\n                \"var\": \"level\",\n                \"value\": \"\"\n            },\n            {\n                \"var\": \"gender\",\n                \"value\": \"\"\n            },\n            {\n                \"var\": \"chapter\",\n                \"value\": \"\"\n            },\n            {\n                \"var\": \"poption\",\n                \"value\": \"\"\n            },\n            {\n                \"var\": \"street1\",\n                \"value\": \"\"\n            },\n            {\n                \"var\": \"street2\",\n                \"value\": \"\"\n            },\n            {\n                \"var\": \"lastName\",\n                \"value\": \"\"\n            },\n            {\n                \"var\": \"firstName\",\n                \"value\": \"\"\n            }\n        ]\n    }],\n    \"template_id\": \"jy7zpl9pwpg5vx6k\"\n}");
-  
-  $headers = array();
-  $headers[] = 'Content-Type: application/json';
-  $headers[] = 'X-Requested-With: XMLHttpRequest';
-  $headers[] = 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYWRiOTEyNGQ2NWE0Zjk1YjNmMTljOTQ2YTNkNjNlZmZlZDcyNDg1ZDg0YjU1OWY3NDBlMzU1ZGQzNzZkODU3ZjkxZjZmMWYxNzFhYzZjYjkiLCJpYXQiOjE2MzYzMTkyMTcuODc5NjY2LCJuYmYiOjE2MzYzMTkyMTcuODc5NjcsImV4cCI6NDc5MTk5MjgxNy44MjY1NjcsInN1YiI6IjE1MjYyIiwic2NvcGVzIjpbImVtYWlsX2Z1bGwiXX0.QRmCze0blgfVdpc__Oti7GZoR5GdNLVF41z3LXGAeL_6OZ_eNTLQdKXS6aP4D0GOqJnJRRTOPRBiLAviD6v5TYceoCaZkvQV1SLSKnrN8pEe-8aiATj5w4dqnNqyPMsm7lRaQ5adBKfytl8RxvXo2Vc9y-5ZjzzXqtIrfT-KsCQ1wOqPdl8Axi8l5aAQ2mOLWRe0ZoqvvW9nb4aMld25jHMmV3PlQXq6LeI9Wd_eZyMQMfBRnQPltOHxRaT_6T14RjUix7tuiKtwsowlWyPEMKJ-dCBGrotfJx_bjgHvs_LfMctqx6HLQR4sidO_QM2kAcQVopOYbbQAlRRbA0rzIrV0sdhYwZ9l9RIQFs-XNOZoq5OO558peh3JWnc1wwSz_Gr3IJJKP6yvrUrb_JTmmOOUY8P8SgOTgfYdvI-aqZhGnnYTPPRI4bX6_FoH7nEplyNQ770PizgLGgz4iLDKjCQTx9CKILrNU1JHpF7eXLZMR_oCS4HE8YWVShS688jDs2kNyitk6ct47VGC6tUUuEyJGOcnbZSUqtGwFBTaUk5tnelLy-DfuCMCN-AJhMrWuEZD4ILh0jR0Mo34Fd2zbrbG5ETZSPz4DFhdyEhWBgEGfq3l0djoQv6MAwjeVTp_tQAoSVpxoaiuz9BqkjA4vQOUnzz6nwJXzQsoRaKHtow';
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  
-  $result = curl_exec($ch);
-  if (curl_errno($ch)) {
-      echo 'Error:' . curl_error($ch);
-  }
-  curl_close($ch);
+curl_setopt($ch, CURLOPT_URL, 'https://api.mailersend.com/v1/email');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "{\n    \"from\": {\n        \"email\": \"noac@lodge104.com\"\n    },\n    \"to\": [\n        {\n            \"email\": \"nickanderson1998@gmail.com\"\n        }\n    ],\n    \"variables\": [{\n        \"email\": \"nickanderson1998@gmail.com\",\n        \"substitutions\": [\n            {\n                \"var\": \"age\",\n                \"value\": \"Test\"\n            },\n            {\n                \"var\": \"dob\",\n                \"value\": \"Test\"\n            },\n            {\n                \"var\": \"zip\",\n                \"value\": \"Test\"\n            },\n            {\n                \"var\": \"city\",\n                \"value\": \"Test\"\n            },\n            {\n                \"var\": \"bsaid\",\n                \"value\": \"Test\"\n            },\n            {\n                \"var\": \"level\",\n                \"value\": \"Test\"\n            },\n            {\n                \"var\": \"gender\",\n                \"value\": \"Test\"\n            },\n            {\n                \"var\": \"chapter\",\n                \"value\": \"Test\"\n            },\n            {\n                \"var\": \"poption\",\n                \"value\": \"Test\"\n            },\n            {\n                \"var\": \"street1\",\n                \"value\": \"Test\"\n            },\n            {\n                \"var\": \"street2\",\n                \"value\": \"Test\"\n            },\n            {\n                \"var\": \"lastName\",\n                \"value\": \"Test\"\n            },\n            {\n                \"var\": \"firstName\",\n                \"value\": \"Test\"\n            }\n        ]\n    }],\n    \"template_id\": \"jy7zpl9pwpg5vx6k\"\n}");
+
+$headers = array();
+$headers[] = 'Content-Type: application/json';
+$headers[] = 'X-Requested-With: XMLHttpRequest';
+$headers[] = 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYWRiOTEyNGQ2NWE0Zjk1YjNmMTljOTQ2YTNkNjNlZmZlZDcyNDg1ZDg0YjU1OWY3NDBlMzU1ZGQzNzZkODU3ZjkxZjZmMWYxNzFhYzZjYjkiLCJpYXQiOjE2MzYzMTkyMTcuODc5NjY2LCJuYmYiOjE2MzYzMTkyMTcuODc5NjcsImV4cCI6NDc5MTk5MjgxNy44MjY1NjcsInN1YiI6IjE1MjYyIiwic2NvcGVzIjpbImVtYWlsX2Z1bGwiXX0.QRmCze0blgfVdpc__Oti7GZoR5GdNLVF41z3LXGAeL_6OZ_eNTLQdKXS6aP4D0GOqJnJRRTOPRBiLAviD6v5TYceoCaZkvQV1SLSKnrN8pEe-8aiATj5w4dqnNqyPMsm7lRaQ5adBKfytl8RxvXo2Vc9y-5ZjzzXqtIrfT-KsCQ1wOqPdl8Axi8l5aAQ2mOLWRe0ZoqvvW9nb4aMld25jHMmV3PlQXq6LeI9Wd_eZyMQMfBRnQPltOHxRaT_6T14RjUix7tuiKtwsowlWyPEMKJ-dCBGrotfJx_bjgHvs_LfMctqx6HLQR4sidO_QM2kAcQVopOYbbQAlRRbA0rzIrV0sdhYwZ9l9RIQFs-XNOZoq5OO558peh3JWnc1wwSz_Gr3IJJKP6yvrUrb_JTmmOOUY8P8SgOTgfYdvI-aqZhGnnYTPPRI4bX6_FoH7nEplyNQ770PizgLGgz4iLDKjCQTx9CKILrNU1JHpF7eXLZMR_oCS4HE8YWVShS688jDs2kNyitk6ct47VGC6tUUuEyJGOcnbZSUqtGwFBTaUk5tnelLy-DfuCMCN-AJhMrWuEZD4ILh0jR0Mo34Fd2zbrbG5ETZSPz4DFhdyEhWBgEGfq3l0djoQv6MAwjeVTp_tQAoSVpxoaiuz9BqkjA4vQOUnzz6nwJXzQsoRaKHtow';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
 
 $createAdult = $conn->prepare("INSERT INTO participants(bsa_id, oalm_id, firstName, lastName, address_line1, address_line2, city, state, zip, email, hphone, cphone, tshirt, text_agreement, gender, chapter, dob, level, payment, aia_check, aia, signature, parent, created, status) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 $createAdult->bind_param("sssssssssssssssssssssssss", $bsa_id, $oalmID, $firstName, $lastName, $address_line1, $address_line2, $city, $state, $zip, $email, $hphone, $cphone, $tshirt, $text, $gender, $chapter, $dob, $level, $payment, $aiacheck, $aia, $signature, $parent, $d, $s);
