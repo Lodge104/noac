bf0ec9b003c077d896d7b4f269b54c681c576af1
Nicholas Anderson <nickanderson1998@gmail.com>
2021-11-06 21:37:13 -0400

    Fix

--- participant/add-application-process.php
+++ participant/add-application-process.php
@@ -58,7 +58,6 @@ $birthDate = explode("-", $dob);
 $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
   ? ((date("Y") - $birthDate[2]) - 1)
   : (date("Y") - $birthDate[2]));
-echo "Age is:" . $age;
 
 $variables = [
   new Variable($email, [
